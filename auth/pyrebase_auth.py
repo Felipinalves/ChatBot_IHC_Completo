dc259242a809cce5038b0873dee226a898333ef7
Felipe Alves Cerquiare <71992399+Felipinalves@users.noreply.github.com>
2025-02-25 23:53:20 -0300

    Update pyrebase_auth.py

--- auth/pyrebase_auth.py
+++ auth/pyrebase_auth.py
@@ -5,13 +5,12 @@ import streamlit as st
 def initialize_pyrebase():
     try:
         firebase_config = {
-            "apiKey": st.secrets["apiKey"],
-            "authDomain": st.secrets["FIREBASE_AUTH_DOMAIN"],
-            "databaseURL": st.secrets["FIREBASE_DATABASE_URL"],
-            "projectId": st.secrets["FIREBASE_PROJECT_ID"],
-            "storageBucket": st.secrets["FIREBASE_STORAGE_BUCKET"],
-            "messagingSenderId": st.secrets["FIREBASE_MESSAGING_SENDER_ID"],
-            "appId": st.secrets["FIREBASE_APP_ID"]
+            "apiKey": st.secrets["FIREBASE"]["apiKey"],
+            "authDomain": st.secrets["FIREBASE"]["authDomain"],
+            "projectId": st.secrets["FIREBASE"]["projectId"],
+            "storageBucket": st.secrets["FIREBASE"]["storageBucket"],
+            "messagingSenderId": st.secrets["FIREBASE"]["messagingSenderId"],
+            "appId": st.secrets["FIREBASE"]["appId"]
         }
         firebase = pyrebase.initialize_app(firebase_config)
         return firebase
