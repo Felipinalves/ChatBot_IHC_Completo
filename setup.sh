bfd8f5c57cb919b59e5ff9204ddbacb36f232b3d
Felipe Alves Cerquiare <71992399+Felipinalves@users.noreply.github.com>
2025-02-26 19:09:00 -0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python -c "import nltk; nltk.download('stopwords'); nltk.download('punkt'); nltk.download('punkt_tab'); nltk.download('averaged_perceptron_tagger'); nltk.download('wordnet')"
