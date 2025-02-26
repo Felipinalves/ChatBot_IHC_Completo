65c7c0bcf91eb1d600314e6fcc20b89331b08f42
Felipe Alves Cerquiare <71992399+Felipinalves@users.noreply.github.com>
2025-02-26 14:23:08 -0300

    Update time_utils.py

--- utils/time_utils.py
+++ utils/time_utils.py
@@ -2,7 +2,7 @@ import datetime
 import pytz
 
 def get_brasilia_time():
-    """Retorna data e hora atual no fuso de Brasília como string ISO."""
+    """Retorna data e hora atual no fuso de Brasília no formato dia/mês/ano hora:minuto."""
     brasilia_tz = pytz.timezone('America/Sao_Paulo')
     now = datetime.datetime.now(brasilia_tz)
-    return now.isoformat()
+    return now.strftime('%d/%m/%Y %H:%M')
