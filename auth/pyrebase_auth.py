b8f2730022fb2d5fac681ccb4cdde00afc5cd32d
Felipe Alves Cerquiare <71992399+Felipinalves@users.noreply.github.com>
2025-02-26 10:06:44 -0300

    Update pyrebase_auth.py

--- auth/pyrebase_auth.py
+++ auth/pyrebase_auth.py
@@ -10,8 +10,7 @@ def initialize_pyrebase():
             "projectId": st.secrets["FIREBASE"]["projectId"],
             "storageBucket": st.secrets["FIREBASE"]["storageBucket"],
             "messagingSenderId": st.secrets["FIREBASE"]["messagingSenderId"],
-            "appId": st.secrets["FIREBASE"]["appId"],
-            "databaseURL": ""
+            "appId": st.secrets["FIREBASE"]["appId"]
         }
         firebase = pyrebase.initialize_app(firebase_config)
         return firebase
